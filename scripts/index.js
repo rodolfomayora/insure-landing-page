7ba5720d0a0c9527a1440d54ef4e12afdddb6af7
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-03 13:51:20 -0400

    ILP-1: add mobile navigation

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,12 @@ let // references
 
 const toggleMobileMenu = () => {
   navigationButton.classList.toggle('header__button--active');
+  mobileMenu.classList.toggle('navigation--active');
 }
 
 const init = () => {
   [navigationButton] = document.getElementsByClassName('js-header__button');
-
+  [mobileMenu] = document.getElementsByClassName('js-navigation');
   navigationButton.addEventListener('click', toggleMobileMenu);
 }
 
